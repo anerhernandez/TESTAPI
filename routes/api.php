6f101998275fde889ef1700e6e77579077cc3d91
anerhernandez <anerhernandezl@gmail.com>
2025-02-11 19:47:40 +0000

    Created user token

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ApiProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('products', ApiProductController::class);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('products', ApiProductController::class);
+});
+
+Route::post("login",[UserController::class,'index']);
